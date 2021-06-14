b029e093e230594a0389e8ce5a047c4c51120223
amit-doxel <amit.doxel@gmail.com>
2021-06-14 10:13:30 +0530

    added orbit control to the scene

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { PerspectiveCamera, Scene, WebGLRenderer } from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 import './style.css';
 
@@ -9,9 +10,12 @@ const app = document.querySelector('#app');
 const scene = new Scene();
 const camera = new PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 10000);
 const renderer = new WebGLRenderer({ antialias: true });
+const controls = new OrbitControls(camera, renderer.domElement);
 
 camera.position.set(0, 0, 5);
 camera.lookAt(mesh.position);
+
+controls.update();
 
 renderer.setSize(innerWidth, innerHeight);
 app?.appendChild(renderer.domElement);
